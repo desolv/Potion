2eb04f57105d5988c847528cb1790d95939b40d7
Michele <michelegje@gmail.com>
2026-01-14 19:46:37 +0000

    Add bot initialization

--- potion.py
+++ potion.py
@@ -1,1 +1,28 @@
+import asyncio
+import os
+import platform
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv(f".env")
+
+bot = commands.Bot(
+    command_prefix="?",
+    help_command=None,
+    intents=discord.Intents.all(),
+)
+
 print("Potion Robot")
+print(
+    f"Running at Python {platform.python_version()}v, "
+    f"Discord.py {discord.__version__}v - {platform.system()} {platform.release()} ({os.name})"
+)
+
+
+async def main():
+    await bot.start(os.getenv("DISCORD_TOKEN"))
+
+
+asyncio.run(main())
