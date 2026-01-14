24237a512d4871963b91b3ddebab9e2bbaf57678
Michele <michelegje@gmail.com>
2026-01-14 20:20:45 +0000

    Add extension loading

--- potion.py
+++ potion.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 import platform
 import sys
+from pathlib import Path
 
 import discord
 from discord.ext import commands
@@ -35,8 +36,22 @@ async def backend():
         sys.exit(1)
 
 
+async def load():
+    for root in "p":
+        for extension in Path(root).rglob("*.py"):
+            if extension.stem.startswith("__") or any(folder in extension.parts for folder in (".venv", "models")):
+                continue
+
+            ext_path = ".".join(extension.with_suffix("").parts)
+            try:
+                await bot.load_extension(ext_path)
+            except commands.NoEntryPointError:
+                continue
+
+
 async def main():
     await backend()
+    await load()
     await bot.start(os.getenv("DISCORD_TOKEN"))
 
 
